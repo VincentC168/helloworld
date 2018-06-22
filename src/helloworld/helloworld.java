fbe68170da02cdd1067a32ab2be1b0750b0c11f6
jiachen <jiachen@KIBU2JXUIW.asiapacific.hpqcorp.net>
2018-06-22 09:25:17 +0800

    first commit

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,13 @@
+package helloworld;
+//import IO.String;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world"); 
+		
+
+	}
+
+}
